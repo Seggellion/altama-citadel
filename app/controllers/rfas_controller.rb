6caa2e67480f4b451a9d8509e7f290fed26ba0e6
dustin@altama.ca <dustin@altama.ca>
2022-03-22 22:11:44 -0700

    Added turbostream status update, and filters to rfa-index

--- app/controllers/rfas_controller.rb
+++ app/controllers/rfas_controller.rb
@@ -1,14 +1,20 @@
 class RfasController < ApplicationController
-  before_action :set_rfa, only: %i[ show edit update destroy ]
+  before_action :set_rfa, only: %i[edit update destroy ]
 
   # GET /rfas or /rfas.json
   def index
     @rfas = Rfa.where(status_id:1)
+    @rfas += Rfa.where(status_id:0)
+    @rfas += Rfa.where(status_id:2)
+    @rfa_hold = Rfa.where(status_id:3)
+    @rfa_solved = Rfa.where(status_id:4)
+    @rfa_unassigned = Rfa.where(status_id:0)
+    @rfa_mine = Rfa.where(user_assigned_id: current_user.id)
+    
   end
 
   # GET /rfas/1 or /rfas/1.json
-  def show
-  end
+
 
   # GET /rfas/new
   def new
@@ -17,7 +23,7 @@ class RfasController < ApplicationController
 
   # GET /rfas/1/edit
   def edit
-    @users = User.all
+    
     @hash =  [*('a'..'z'),*('0'..'9')].shuffle[0,8].join
   end
 
@@ -26,13 +32,12 @@ class RfasController < ApplicationController
     @rfa = Rfa.new(rfa_params)
     @location = Location.find_by_id(@rfa.location_id)
   
-    Discord::Notifier.message('Discord Notifier Webhook Notification')
-
+    Discord::Notifier.message('Request for assistance at:' + @location.name)
 
     respond_to do |format|
       if @rfa.save
         format.html { redirect_to rfa_location_path(location: @location), notice: "Rfa was successfully created." }
-        format.json { render :show, status: :created, location: @rfa }
+        #format.json { render :show, status: :created, location: @rfa }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @rfa.errors, status: :unprocessable_entity }
@@ -46,14 +51,31 @@ class RfasController < ApplicationController
     # Discord::Notifier.message('!update user: <@'+ user.uid + '>' )
     status = Rfa.get_status(params[:rfa][:status_id].to_i) 
     Discord::Notifier.message('!update ,'+ user.uid + ',' + status)
+  #  assigned_user = User.find_by_id(rfa_params[:user_assigned_id])
+  merge_params = rfa_params
+  if rfa_params[:user_assigned_id].blank?
+    merge_params = rfa_params.merge(status_id: 0)
+  end
     
+
     respond_to do |format|
-      if @rfa.update(rfa_params)
+      if @rfa.update(merge_params)
+         #format.turbo_stream { render turbo_stream: turbo_stream.update(@rfa) }
+       format.turbo_stream
+       
+      #  format.turbo_stream { render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@rfa)}_item") }
+
+       # format.turbo_stream do
+       #   render turbo_stream: turbo_stream.replace(@rfa, partial: 'web/rfa-status')
+      # end
         format.html { redirect_to edit_rfa_path(@rfa), notice: "Rfa was successfully updated." }
-        format.json { render :show, status: :ok, location: @rfa }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @rfa.errors, status: :unprocessable_entity }
+        
+        format.html { redirect_to edit_rfa_path(@rfa), notice: "Rfa must be assigned to" }
+        
+        # format.html { render :edit, status: :unprocessable_entity }
+        
+        #format.json { render json: @rfa.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -71,10 +93,11 @@ class RfasController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_rfa
       @rfa = Rfa.find(params[:id])
+      @users = User.all
     end
 
     # Only allow a list of trusted parameters through.
-    def rfa_params
+    def rfa_params      
       params.require(:rfa).permit(:title, :description, :rsi_username, :status_id, 
       :location_id, :ship_id, :priority_id, :total_fuel, :total_price, :total_cost, 
       :aec_rewards, :user_assigned_id, :user_id)      
--- app/views/rfas/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.partial! "rfas/rfa", rfa: @rfa
