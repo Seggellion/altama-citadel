f464b126dc9d2d47b05cd585a8c2e06b1699006a
dustin@altama.ca <dustin@altama.ca>
2022-04-10 23:07:39 -0700

    added color hue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_02_053843) do
+ActiveRecord::Schema.define(version: 2022_04_11_052927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,6 +131,8 @@ ActiveRecord::Schema.define(version: 2022_04_02_053843) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "starfarer_image"
+    t.string "ocean_color"
+    t.string "surface_color"
   end
 
   create_table "manufacturers", force: :cascade do |t|
