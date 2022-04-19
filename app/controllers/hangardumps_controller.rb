b2ef24c282fc3231b21557b6145eff862e8042c8
Ariana Wojcik <ariana@maidencode.com>
2022-04-19 01:22:21 -0500

    uploader update

--- app/controllers/hangardumps_controller.rb
+++ app/controllers/hangardumps_controller.rb
@@ -1,4 +1,8 @@
 class HangardumpsController < ApplicationController
+  require 'json'
+  require 'net/http'
+  require 'uri' 
+
   def index
     @hangardumps = Hangardump.all
   end
@@ -7,10 +11,42 @@ class HangardumpsController < ApplicationController
     @hangardump = Hangardump.new
   end
 
+  def fetch(uri_str, limit = 10)
+    # You should choose better exception.
+    raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+  
+    url = URI.parse(uri_str)
+    req = Net::HTTP::Get.new(url.path, { 'User-Agent' => 'Mozilla/5.0 (etc...)' })
+    response = Net::HTTP.start(url.host, url.port, use_ssl: false) { |http| http.request(req) }
+    case response
+    when Net::HTTPSuccess     then response
+    when Net::HTTPRedirection then fetch(response['location'], limit - 1)
+    else
+      response.error!
+    end
+  end
+
+
   def create
     @hangardump = Hangardump.new(hangardump_params)
     if @hangardump.save
       redirect_to hangardumps_path, notice: "The hangardump has been uploaded."
+      @url_string = url_for(@hangardump.attachment)
+      puts(@url_string)
+      
+      response = fetch(@url_string)
+      puts(response.body)
+      data_hash = JSON.parse(response.body)
+
+      #file = Net::HTTP.get_response(URI.parse(@url_string)).body
+      #data_hash = JSON.parse(file)
+      
+      
+      
+      #json_file = StringIO.new(@url_string)
+      #file = json_file.read
+      #data_hash = JSON.parse(file)
+      puts(JSON.dump(data_hash))
     else
       render "new"
     end
@@ -24,6 +60,6 @@ class HangardumpsController < ApplicationController
 
   private
     def hangardump_params
-    params.require(:hangardump).permit(:name, attachment: [])
+    params.require(:hangardump).permit(:name, :attachment)
   end
 end
