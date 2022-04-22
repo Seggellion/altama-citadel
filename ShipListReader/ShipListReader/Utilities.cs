0552b5635890f0f517e6430d18c2b8ab927fc988
Ariana Wojcik <ariana@maidencode.com>
2022-04-22 01:25:16 -0500

    checkin ShipListReader(built in visualstudio2019)

---
+++ ShipListReader/ShipListReader/Utilities.cs
@@ -0,0 +1,73 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipListReader
+{
+    public class Utilities
+    {
+        private static Logger Log = LogManager.GetCurrentClassLogger();
+        public static FileInfo GetSourceFile()
+        {
+            var sourceFile = new FileInfo("temp");
+            DirectoryInfo info = new DirectoryInfo(ConfigurationManager.AppSettings["FilesDir"]);
+            FileInfo[] files = info.GetFiles().OrderBy(p => p.CreationTime).ToArray();
+            foreach (FileInfo file in files)
+            {
+                if (file.Name.Equals(ConfigurationManager.AppSettings["SourceFileName"]))
+                {
+                    Log.Info("Found ship list file to process, filename is {0}", file.Name);
+                    return file;
+                }
+            }
+            return null;
+        }
+
+        public static string GetSeedsFileLine(Dictionary<string, string> seedInfo)
+        {
+            var sb = new StringBuilder();
+            //RsiUser.create(name: 'OPSCHIEF', title: 'Member-Owner', link: '')
+            // manufacturers: SELECT id, name, description, origin_location, created_at, updated_at, logo FROM public.manufacturers;
+
+            Dictionary<string, string>.KeyCollection keys = seedInfo.Keys;
+            sb.Append("Ships.create(");
+            bool isFirst = true;
+            foreach (string columnName in keys)
+            {
+                if (!isFirst)
+                    sb.Append(", ");
+                sb.Append(columnName);
+                sb.Append(": '");
+                sb.Append(seedInfo.GetValueOrDefault(columnName));
+                sb.Append("'");
+                isFirst = false;
+            }
+            sb.Append(")");
+            return sb.ToString();
+        }
+
+        public static string GetMFRFileLine(Dictionary<int, string> mfrInfo)
+        {
+            var sb = new StringBuilder();
+            //RsiUser.create(name: 'OPSCHIEF', title: 'Member-Owner', link: '')
+            // manufacturers: SELECT id, name, description, origin_location, created_at, updated_at, logo FROM public.manufacturers;
+
+            Dictionary<int, string>.KeyCollection keys = mfrInfo.Keys;
+            foreach (int key in keys)
+            {
+                sb.Append("Manufacturers.create(");
+                sb.Append("id: '");
+                sb.Append(key);
+                sb.Append("', name: '");
+                sb.Append(mfrInfo.GetValueOrDefault(key));
+                sb.AppendLine("', description: '', origin_location: '', created_at: '', updated_at: '', logo: '')");
+            }
+            return sb.ToString();
+        }
+    }
+}
