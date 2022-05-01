23c903fb9b1be89124f57b06f9d5fb7e720cf184
tevye cowan <tevye.cowan@shopify.com>
2022-05-01 14:13:54 -0700

    updating guildstone controllers with basic defs

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,19 +1,61 @@
 class RulesController < ApplicationController
 
+  before_action :set_rule, only: %i[ show edit update destroy ]
 
   def show
   end
 
   def index
+    @rules = Rule.all
+  end
+
+  def new
+    @rule = Rule.new
   end
 
   def create
+    @rule = Rule.new(rule_params)
+
+    respond_to do |format|
+      if @rule.save
+        format.html { redirect_to @rule, notice: "Rule was successfully created." }
+        format.json { render :show, status: :created, location: @rule }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @rule.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @rule.destroy
+    respond_to do |format|
+      format.html { redirect_to rules_url, notice: "Rule was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   def update
+    respond_to do |format|
+      if @rule.update(rule_params)
+        format.html { redirect_to @rule, notice: "Rule was successfully updated." }
+        format.json { render :show, status: :ok, location: @rule }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @rule.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_rule
+    @rule = Rule.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def rule_params
+    params.fetch(:rule, {})
   end
 
   
