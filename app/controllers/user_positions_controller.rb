826914d6aefd84e8d98e20f9599d9a85feabfe3f
tevye cowan <tevye.cowan@shopify.com>
2022-05-01 13:50:59 -0700

    structured guildstone files (still no defs etc)

---
+++ app/controllers/user_positions_controller.rb
@@ -0,0 +1,21 @@
+class UserPositionsController < ApplicationController
+
+
+  def show
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  
+
+end
