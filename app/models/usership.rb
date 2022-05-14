473f7c6756c0b46441dcef4b1518e1fd688a114d
Ariana Wojcik <ariana@maidencode.com>
2022-05-14 03:02:55 -0500

    create of userships fixed

--- app/models/usership.rb
+++ app/models/usership.rb
@@ -5,14 +5,4 @@ class Usership < ApplicationRecord
     has_one_attached :attachment
     include ActiveModel::Serializers::JSON
 
-    def attributes=(hash)
-        hash.each do |key, value|
-          send("#{key}=", value)
-        end
-    end   
-
-    def attributes
-        instance_values
-    end    
-
 end
