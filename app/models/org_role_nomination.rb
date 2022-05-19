bbd378b491d16f0b764153cc08ba264794e980c3
Shopify <shopify@Shopifys-MBP.hitronhub.home>
2022-05-19 11:07:14 -0700

    refactoring model

--- app/models/org_role_nomination.rb
+++
@@ -1,7 +0,0 @@
-class OrgRoleNomination < ApplicationRecord
-
-    def total_votes
-        OrgRoleVote.where(org_role_nomination_id:self.id).count
-    end
-
-end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,14 +1,31 @@
 class Position < ApplicationRecord
   belongs_to :department
-  
   has_many :position_nominations
-  has_one :user, :through => :user_position
+  
+  def user
+    unless self.position_nominations.empty?
+      self.position_nominations.first.user
+    end
+  end
 
+  def user_position
+    UserPosition.find_by(position_id: self.id)
+  end
+
+  def nomination(user)
+    if user
+   PositionNomination.where(nominee_id: user.id, position_id: self.id)
+    end
+  end
+
+  def application(user)
+    PositionApplication.where(nominee_id: user.id, position_id: self.id)
+  end
 
   def position_username
-    user_position = UserPosition.find_by(position_id: self.id)
-    unless user_position.nil?
-      user_position.user.username
+
+    unless self.user.nil?
+      self.user.username
     else
       puts "Apply today!"
     end
