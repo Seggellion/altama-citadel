45f5c8eef04da222ab613db085e68c098ac82d14
tevye cowan <tevye.cowan@shopify.com>
2022-05-22 12:53:49 -0700

    updated position nomination modal

--- app/models/position_nomination.rb
+++ app/models/position_nomination.rb
@@ -1,4 +1,14 @@
 class PositionNomination < ApplicationRecord
   belongs_to :user, foreign_key: 'nominator_id'
   has_one :position
+
+  def user_nominated?
+   # PositionNomination.find_by(position_id: self.id)
+    #PositionNomination.find_by(nominee_id: self.user.id, position_id: self.id)
+    #@position_nomination = PositionNomination.where(nominee_id: current_user, position_id: self.id)
+  
+  end
+
+
+
 end
