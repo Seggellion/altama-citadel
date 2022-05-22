eb0f81b8cf1ed8e4bca4153dac7bf6a429ba51a4
Shopify <shopify@Shopifys-MacBook-Pro.local>
2022-05-22 14:13:47 -0700

    added buttons and links animations css

--- app/javascript/controllers/guildstone_controller.js
+++ app/javascript/controllers/guildstone_controller.js
@@ -2,14 +2,64 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
- static targets = ["mainmenu"]
+ static targets = ["mainmenu", "createPosition", "createDepartment", "createNomination", "createRule"]
 
   close(event) {
-
-
 //event.target.
+  }
 
+  createPosition(event){
+    var button =  event.currentTarget;
+    button.classList.toggle("click");
+    var active_elements = document.querySelectorAll('.active');
+    active_elements.forEach(el => {
+      el.classList.remove('active');
+    });
+    this.createPositionTarget.classList.toggle("active");
+    setTimeout(function(){
+      button.classList.toggle("click");
+    },100);
+  }
 
+  createDepartment(event){
+    var button =  event.currentTarget;
+    button.classList.toggle("click");
+    var active_elements = document.querySelectorAll('.active');
+    active_elements.forEach(el => {
+      el.classList.remove('active');
+    });
+    this.createDepartmentTarget.classList.toggle("active");
+    setTimeout(function(){
+      button.classList.toggle("click");
+    },100);
+  }
+
+  createNomination(event){
+    var button =  event.currentTarget;
+    button.classList.toggle("click");
+    var active_elements = document.querySelectorAll('.active');
+    active_elements.forEach(el => {
+      el.classList.remove('active');
+    });
+    this.createNominationTarget.classList.toggle("active");
+    
+    setTimeout(function(){
+      button.classList.toggle("click");
+    },100);
+  }
+
+  createRule(event){
+    var button =  event.currentTarget;
+    button.classList.toggle("click");
+    var active_elements = document.querySelectorAll('.active');
+    active_elements.forEach(el => {
+      el.classList.remove('active');
+    });
+    this.createRuleTarget.classList.toggle("active");
+    
+    setTimeout(function(){
+      button.classList.toggle("click");
+    },100);
   }
   
 }
