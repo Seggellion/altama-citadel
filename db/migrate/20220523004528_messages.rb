396d087f809c6f6ea22774281df02c4fc9a83af3
Shopify <shopify@Shopifys-MacBook-Pro.local>
2022-05-22 18:09:06 -0700

    switch

--- db/migrate/20220523004528_messages.rb
+++ db/migrate/20220523004528_messages.rb
@@ -1,10 +1,11 @@
 class Messages < ActiveRecord::Migration[7.0]
   def change
-    create_table :votes do |t|
+    create_table :messages do |t|
       t.integer :user_id
       t.integer :sender_id
       t.integer :task_id
       t.string :content
+      t.boolean :read
       t.string :subject
       t.timestamps
     end
