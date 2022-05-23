cb6868426a69eeee6850381ecd98f4b3f366335f
Shopify <shopify@Shopifys-MacBook-Pro.local>
2022-05-22 18:24:49 -0700

    hello

--- app/controllers/guildstones_controller.rb
+++ app/controllers/guildstones_controller.rb
@@ -21,6 +21,10 @@ class GuildstonesController < ApplicationController
     @users = User.all
     @positions = Position.all
     @position_nominations = PositionNomination.all
+    
+    @user_position_windows =  @window_states.include?("UserPositions")
+    
+    
 
   end
 
