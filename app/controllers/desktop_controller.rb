ff7a1d523ae2430631ff3f8460bb65194faee58d
Old Shopify computer <oldshopifycomputer@Olds-MacBook-Pro.local>
2022-09-18 16:31:25 -0400

    further refinement

--- app/controllers/desktop_controller.rb
+++ app/controllers/desktop_controller.rb
@@ -32,7 +32,7 @@ end
   @hash =  [*('a'..'z'),*('0'..'9')].shuffle[0,8].join
   @myfleetships = current_user.userships.where(show_information:true)
   @all_events = Event.all
-  @timeline_events = Event.where.not(event_type:1)
+  @timeline_events = Event.where(event_type:nil)
   current_user.desktop
 # Discord::Notifier.message('Discord Notifier Webhook Notification')
 end
