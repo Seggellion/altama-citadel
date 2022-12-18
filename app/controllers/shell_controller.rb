02ad6f8d639e5f175b25d600cf7c17daecf1562e
Old Shopify computer <oldshopifycomputer@Olds-MacBook-Pro.local>
2022-12-18 18:42:00 -0500

    added directory

--- app/controllers/shell_controller.rb
+++ app/controllers/shell_controller.rb
@@ -1,25 +1,37 @@
 class ShellController < ApplicationController
 
 def command_entry
+  
+  if params[:query].split.size > 1
+    filename = params[:query].split.last
+  end
 command = params[:query].split.first
-filename = params[:query].split.last
+
 @task_manager = TaskManager.find_by(user_id: current_user)
 @all_tasks = Task.where(task_manager_id: @task_manager.id)
 @task = @all_tasks.find_by(task_manager_id: @task_manager.id, name: "Altama Shell")
 
 
 case command
+  when "ls"
+    
+  # directory = Dir["app/views/desktop/apps/shell_apps/files/*"]
+  # directory = Dir["app/views/desktop/apps/shell_apps/files/*"].select { |f| File.file? File.join("your/folder", f) }
+  #directory = Dir.glob('app/views/desktop/apps/shell_apps/files/*').select { |e| File.file? e }
+  directory = Dir[ 'app/views/desktop/apps/shell_apps/files/*' ].select{ |f| File.file? f }.map{ |f| File.basename f }
+
+   @task.update(state:directory.prepend("LS"))
   when "ping?"
     @task.update(state:'Pong!')
   when "clear"
     @task.update(state:'clear')
   when "vim"    
     @task.update(state:'vim')
-  when "moonstone"
-  
-    if filename
+  when "moonstone"  
     
-    text = File.read("app/views/desktop/apps/shell_apps/files/#{filename}")
+    if filename    
+      file_content = File.read("app/views/desktop/apps/shell_apps/files/#{filename}")
+      Moonstone.compile(file_content)
     else
       text = "No file specified"
     end
