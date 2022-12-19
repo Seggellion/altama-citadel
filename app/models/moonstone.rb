070f0799b8161954c733e896e9a902238027903b
Old Shopify computer <oldshopifycomputer@Olds-MacBook-Pro.local>
2022-12-18 19:00:30 -0500

    model error

--- app/models/moonstone.rb
+++ app/models/moonstone.rb
@@ -1,4 +1,4 @@
-class Badge 
+class Moonstone 
     def compile(file_contents)
         
         @task =  Task.new(name: 'Moonstone',task_manager_id: task_manager.id, view: 'window')
@@ -11,5 +11,6 @@ class Badge
             format.html { render :index, status: :unprocessable_entity }
             format.json { render json: @task.errors, status: :unprocessable_entity }
           end
+        end
     end
 end
