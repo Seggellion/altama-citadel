b768b8eaa2575040986193caf725829469a48aa7
Old Shopify computer <oldshopifycomputer@Olds-MBP.hitronhub.home>
2023-01-14 19:06:59 -0800

    added global bsod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  rescue_from StandardError, with: :handle_error if Rails.env.production?
+
+  #error handler
+  def handle_error(e)
+    redirect_to bsod_path 
+    #code to send email and redirect to error page
+  end
+
+
     def require_login
         unless current_user
           redirect_to root_url
