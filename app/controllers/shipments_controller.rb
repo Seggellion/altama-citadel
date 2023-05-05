fb73e11d1d087adb883fae14fbcbcd30d99bdbd3
Old Shopify computer <oldshopifycomputer@Olds-MBP.hitronhub.home>
2023-05-04 20:44:00 -0700

    changed require login for API

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,5 +1,7 @@
 class ShipmentsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:json_request]
+  skip_before_action :require_login, only: [:json_request]
+
   
   
   def json_request
