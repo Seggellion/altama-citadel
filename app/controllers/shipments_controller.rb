320cdffe76460a9a26c8605a6708e340c3f9ee90
Old Shopify computer <oldshopifycomputer@Olds-MBP.hitronhub.home>
2023-05-29 21:20:51 -0700

    fixed bug with shipments using floats instead of ints

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -44,7 +44,7 @@ sell_commodity = Commodity.search_by_name_and_location(sell_search_query).order(
     # Check if records exist in the database
     records_exist = buy_commodity.present? && sell_commodity.present?
 if records_exist
-  total_profit = ( sell_commodity.buy.to_f * total_units) - ( buy_commodity.sell.to_f * total_units)
+  total_profit = ( sell_commodity.buy.to_i * total_units.to_i) - ( buy_commodity.sell.to_i * total_units.to_i)
   response = {total_profit:  total_profit  }
 else
   response = {total_profit:  'invalid' }
