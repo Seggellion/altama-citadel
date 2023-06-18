cdc5b81584add03492e1b033cee56cc5e7c0ed58
Old Shopify computer <oldshopifycomputer@Olds-MacBook-Pro.local>
2023-06-18 13:16:31 -0700

    maybe fixed bugs

--- app/controllers/position_nominations_controller.rb
+++ app/controllers/position_nominations_controller.rb
@@ -56,7 +56,7 @@ end
   def destroy
     @position_nomination.destroy
     respond_to do |format|
-      format.html { redirect_to @guildstone, notice: "Position Nomination was successfully destroyed." }
+      format.html { redirect_to root_path, notice: "Position Nomination was successfully destroyed." }
       format.json { head :no_content }
     end
   end
