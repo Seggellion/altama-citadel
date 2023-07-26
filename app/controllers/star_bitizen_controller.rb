94851d3ced35bb7dedb1a1fe9eccf4055a5aef15
Old Shopify computer <oldshopifycomputer@Olds-MBP.hitronhub.home>
2023-07-25 21:23:43 -0700

    refactored star bitizen controller

--- app/controllers/star_bitizen_controller.rb
+++ app/controllers/star_bitizen_controller.rb
@@ -1,152 +1,132 @@
 class StarBitizenController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
-  def buy_trade
-    json_request = JSON.parse(request.body.read)
-    @secretguid  = ENV['STARBITIZEN_EXCHANGE']
-    commodity_name = json_request["commodity"]
-    received_guid = json_request["secretguid"]  
-    starbits = json_request["starbits"].to_i
-    
-    return unless @secretguid == received_guid  
-    @active_commodities = Commodity.where(active:true)
-    player_name = json_request["player_name"]
-    from_location = json_request["from_location"]
-    to_location = json_request["to_location"]
+  before_action :parse_json_request, :validate_secret_guid
 
-    
-    # sell_commodity = @active_commodities.search_by_name_and_location(sell_search_query).min_by { |commodity| (commodity.updated_at - Time.current).abs }
-    sell_search_query = "#{commodity_name} #{to_location}"
-    sell_commodity = @active_commodities.search_by_name_and_location(sell_search_query).where('buy > ?', 0).first
-    buy_search_query = "#{commodity_name} #{from_location}"
-    buy_commodity = @active_commodities.search_by_name_and_location(buy_search_query).where('sell > ?', 0).first
+  def buy_trade    
+    setup_trade_variables    
+    records_exist = @buy_commodity.present? && @sell_commodity.present?
+    response = handle_buy_trade(records_exist, @buy_commodity, @total_units, @to_user, @starbits)
+    render json: response
+  rescue => e
+    logger.error "Exception in buy_trade: #{e.message}"
+    render json: { capital: '30000 ERROR' }, status: 500
+  end
 
-    total_units = json_request["total_units"].to_i
-    to_user = User.where("lower(username) LIKE lower(?)", "%#{player_name.downcase}%").first_or_initialize
-  
-    if to_user.new_record?      
-      to_user.username = player_name
-      to_user.password = SecureRandom.hex(10) 
-      to_user.provider = 'StarBitizen' # replace with actual values
-      to_user.save!      
-    end
-  
-   
-   # buy_commodity = @active_commodities.search_by_name_and_location(buy_search_query).min_by { |commodity| (commodity.updated_at - Time.current).abs }
-    
-    to_user_id = User.where('lower(username) = lower(?)', player_name).first.id  
-  
-    records_exist = buy_commodity.present? && sell_commodity.present?
-    
-    if records_exist &&  buy_commodity.inventory > 0
-      actual_removed = [buy_commodity.maxInventory, total_units].min
-      actual_removed = [buy_commodity.inventory, actual_removed].min
-  
-      # Now calculate the capital
-      capital = buy_commodity.sell.to_i * actual_removed
-  
-      if capital <= starbits
-        buy_commodity_inventory = buy_commodity.decrement!(:inventory, actual_removed)    
-        buy_commodity.save
-        total_profit = 0
-        existing_run = StarBitizenRun.find_by(user_id: to_user_id, profit:0)
-  
-        if existing_run && existing_run.created_at < (Time.now - 4.minutes)      
-          existing_run.destroy
-          StarBitizenRun.create(
-            commodity_id: buy_commodity.id,
-            user_id: to_user_id, 
-            profit: total_profit,
-            scu: actual_removed)   
-        else
-          StarBitizenRun.create(
-            commodity_id: buy_commodity.id,
-            user_id: to_user_id, 
-            profit: total_profit,
-            scu: actual_removed)
-        end
-  
-        response = {capital:  capital  }
-      else
-        response = {capital:  'insufficient_funds' }
-      end
-    elsif records_exist &&  buy_commodity.inventory == 0
-      response = {capital:  'insufficient_inventory' }
-    else
-      
-      response = {capital:  'invalid' }
-    end
-    
+  def sell_trade
+    @sell_commodity = find_sell_commodity
+    records_exist = @sell_commodity.present?
+    @current_run = find_current_run
+    response = handle_sell_trade(records_exist, @current_run, @sell_commodity)
     render json: response
   end
-  
 
+  private
 
-def sell_trade
-  json_request = JSON.parse(request.body.read)
-  @secretguid  = ENV['STARBITIZEN_EXCHANGE']
-  received_guid = json_request["secretguid"]  
-  
-  return unless @secretguid == received_guid  
-  player_name = json_request["player_name"]  
-  
-  to_user_id = User.find_by(username: player_name)
-  commodity_name = json_request["commodity"]
-  @active_commodities = Commodity.where(active:true)
+  def handle_buy_trade(records_exist, buy_commodity, total_units, to_user, starbits)
+    response = {capital:  '30000 ERROR'}
+    if records_exist && buy_commodity.inventory > 0
+        actual_removed = [buy_commodity.maxInventory, total_units].min
+        actual_removed = [buy_commodity.inventory, actual_removed].min
 
-  to_location = json_request["to_location"]
-  total_units = json_request["total_units"].to_i
-  sell_search_query = "#{commodity_name} #{to_location}"
+        # Now calculate the capital
+        capital = buy_commodity.sell.to_i * actual_removed
 
-#is it possible sell_commodity does not exist?
+        if capital <= starbits
+            buy_commodity_inventory = buy_commodity.decrement!(:inventory, actual_removed)    
+            buy_commodity.save
+            total_profit = 0
+            existing_run = StarBitizenRun.find_by(user_id: to_user.id, profit:0)
 
-  sell_commodity =  @active_commodities.search_by_name_and_location(sell_search_query).min_by { |commodity| (commodity.updated_at - Time.current).abs }
-  records_exist = sell_commodity.present?
-  current_run = StarBitizenRun.find_by(user_id: to_user_id, profit: 0) 
+            existing_run = StarBitizenRun.where(user_id: to_user.id, profit:0)
 
-  if records_exist &&  current_run
-    total_units = current_run.scu
-    capital = current_run.commodity.sell.to_i * total_units
-    total_profit = ( sell_commodity.buy.to_i * total_units) - ( capital)
-    total_sold = total_profit + capital
-    current_run.update(profit:total_profit)
+            if existing_run.present? 
+                existing_run.destroy_all
+            end
+            StarBitizenRun.create(commodity_id: buy_commodity.id, user_id: to_user.id, profit: total_profit, scu: actual_removed)          
+            response = {capital:  capital }
+        else
+            response = {capital:  'insufficient_funds' }
+        end
+    elsif records_exist &&  buy_commodity.inventory == 0
+        response = {capital:  'insufficient_inventory' }
+    else
+        response = {capital:  'invalid' }
+    end
 
-    response = {total_sold:  total_sold}
-  else
-    response = {total_sold:  'invalid'  }
-  end
-
-  render json: response
-
+    return response.to_json
 end
 
 
-def profit_check
-    json_request = JSON.parse(request.body.read)
-    commodity_name = json_request["commodity"]
-    from_location = json_request["from_location"]
-    to_location = json_request["to_location"]
-    total_units = json_request["total_units"]
-
-
-  buy_search_query = "#{commodity_name} #{from_location}"
-  buy_commodity = @active_commodities.search_by_name_and_location(buy_search_query).min_by { |commodity| (commodity.updated_at - Time.current).abs }
-  sell_search_query = "#{commodity_name} #{to_location}"
-  sell_commodity = @active_commodities.search_by_name_and_location(sell_search_query).min_by { |commodity| (commodity.updated_at - Time.current).abs }
-
-    records_exist = buy_commodity.present? && sell_commodity.present?
-    if records_exist
-      total_profit = ( sell_commodity.buy.to_i * total_units.to_i) - ( buy_commodity.sell.to_i * total_units.to_i)
-      response = {total_profit:  total_profit  }
+  def handle_sell_trade(records_exist, current_run, sell_commodity)
+    response = {total_sold: '30000 ERROR'}
+    
+    if records_exist && current_run
+      total_units = current_run.scu
+      buy_commodity = current_run.commodity
+      capital = buy_commodity.sell * total_units
+      total_profit = (sell_commodity.buy.to_i * total_units) - capital
+      total_sold = sell_commodity.buy.to_i * total_units
+      
+      current_run.update(profit: total_profit)
+      response = { total_sold: total_sold }
     else
-      response = {total_profit:  'invalid' }
+      response = { total_sold: 'invalid' }
     end
+    response.to_json
+  end
+  
 
-
-    # Render the response
-    render json: response
+  def parse_json_request
+    @json_request ||= JSON.parse(request.body.read)
   end
 
+  def validate_secret_guid
+    @secretguid = ENV['STARBITIZEN_EXCHANGE']
+    received_guid = @json_request["secretguid"]
 
+    unless @secretguid == received_guid
+      render json: { error: 'Invalid secret guid' }, status: :unauthorized
+      return
+    end
+  end
 
+  def setup_trade_variables
+    @commodity_name = @json_request["commodity"]
+    @starbits = @json_request["starbits"].to_i
+    player_name = @json_request["player_name"]
+    from_location = @json_request["from_location"]
+    to_location = @json_request["to_location"]
+
+    sell_search_query = "#{@commodity_name} #{to_location}"
+    @sell_commodity = Commodity.search_by_name_and_location(sell_search_query).where('buy > ?', 0).first
+    buy_search_query = "#{@commodity_name} #{from_location}"
+    @buy_commodity = Commodity.search_by_name_and_location(buy_search_query).where('sell > ?', 0).first
+    @total_units = @json_request["total_units"].to_i
+    @to_user = fetch_or_create_user(player_name)
+  end
+
+  def find_sell_commodity    
+    commodity_name = @json_request["commodity"]
+    to_location = @json_request["to_location"]
+    sell_search_query = "#{commodity_name} #{to_location}"
+    Commodity.where(active: true).search_by_name_and_location(sell_search_query).where('buy > ?', 0).first
+  end
+
+  def find_current_run
+    player_name = @json_request["player_name"]
+    to_user_id = User.find_by(username: player_name)
+    StarBitizenRun.find_by(user_id: to_user_id, profit: 0)
+  end
+
+  def fetch_or_create_user(player_name)
+    user = User.where("lower(username) = lower(?)", player_name).first_or_initialize do |new_user|
+      new_user.username = player_name
+      new_user.password = SecureRandom.hex(10)
+      new_user.provider = 'StarBitizen'
+    end
+    user.save! if user.new_record?
+    user
+  end
+
+  
 end
