4bc16f3338d86e0d340fee3f5ddb2a47ae5438dc
Old Shopify computer <oldshopifycomputer@Olds-MacBook-Pro.local>
2023-09-12 19:22:53 -0700

    added parameter to component api

--- app/controllers/ship_components_controller.rb
+++ app/controllers/ship_components_controller.rb
@@ -1,6 +1,10 @@
 class ShipComponentsController < ApplicationController
     def index
-      @ship_components = ShipComponent.all
+        if params[:type]
+            @ship_components = ShipComponent.where(component_type: params[:type])
+          else
+            @ship_components = ShipComponent.all
+          end      
       render json: @ship_components
     end
   end
