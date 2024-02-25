6b5a66d77e235d80a69afeef80dd3c0a5bbb75d8
Seggellion <dustin@altama.ca>
2024-02-24 19:50:20 -0800

    added location parameters to api

--- app/resources/location_resource.rb
+++ app/resources/location_resource.rb
@@ -1,4 +1,4 @@
 class LocationResource < JSONAPI::Resource
-    attributes :name, :location_type, :parent, :ammenities_fuel, :ammenities_repair, :ammenities_rearm, :trade_terminal, :system, :mass, ;apoapsis, :periapsis
+    attributes :name, :location_type, :parent, :ammenities_fuel, :ammenities_repair, :ammenities_rearm, :trade_terminal, :system, :mass, :apoapsis, :periapsis
 
 end
